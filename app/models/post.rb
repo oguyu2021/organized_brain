5128501233541c21e606bd8ac69a84f3645739d8
ogurayusuke <oguyu2021@gmail.com>
2023-08-02 17:38:53 +0900

    issue3完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :user
   enum priority: { 高: 0, 中: 1, 低: 2 }
   enum category: { 生活: 0, 仕事: 1, 健康: 2 }
 
   scope :sorted_by_priority, -> { order(priority: :asc) }
+  scope :search, ->(query) { ransack(title_or_content_cont: query).result } 
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title", "content"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :posts
+  resources :users
+  resources :posts do
+    collection do
+      get 'search'
+    end
+  end
   root 'posts#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"

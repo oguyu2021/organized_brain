00457775091aff14989945e429a7d1f24c2ec48a
ogurayusuke <oguyu2021@gmail.com>
2023-08-12 08:04:19 +0900

    エラー画面設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
   belongs_to :user
   enum priority: { 高: 0, 中: 1, 低: 2 }
   enum category: { 生活: 0, 仕事: 1, 健康: 2 }
